a89e345db625102fe0b5506e6b54eb36ee428cfa
Mafialate <mafialate@ukr.net>
2026-01-22 18:30:57 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,12 +1,40 @@
 from abc import ABC
+from typing import Type
 
 
 class IntegerRange:
-    pass
+    def __init__(self, min_amount: int, max_amount: int) -> None:
+        self.min_amount = min_amount
+        self.max_amount = max_amount
+
+    def __set_name__(self, owner: object, name: str) -> None:
+        self.protected_name = "_" + name
+
+    def __get__(self, obj: object, obj_type: type = None) -> int:
+        return getattr(obj, self.protected_name)
+
+    def __set__(self, obj: object, value: int) -> None:
+        if isinstance(value, int):
+            if self.min_amount <= value <= self.max_amount:
+                setattr(obj, self.protected_name, value)
+            else:
+                raise ValueError()
+        else:
+            raise TypeError ()
 
 
 class Visitor:
-   pass
+   def __init__(
+           self,
+           name: str,
+           age: int,
+           weight: int,
+           height: int
+   ) -> None:
+       self.name = name
+       self.age = age
+       self.weight = weight
+       self.height = height
 
 
 class SlideLimitationValidator(ABC):
@@ -14,12 +42,32 @@ class SlideLimitationValidator(ABC):
 
 
 class ChildrenSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(4, 14)
+    height = IntegerRange(80, 120)
+    weight = IntegerRange(20, 50)
 
 
 class AdultSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(14, 60)
+    height = IntegerRange(120, 220)
+    weight = IntegerRange(50, 120)
 
 
 class Slide:
-    pass
+    def __init__(
+        self,
+        name: str,
+        limitation_class: Type[SlideLimitationValidator]
+    ) -> None:
+        self.name = name
+        self.limitation_class = limitation_class
+
+    def can_access(self, visitor: Visitor) -> bool:
+        try:
+            limitation_validator = self.limitation_class()
+            limitation_validator.age = visitor.age
+            limitation_validator.height = visitor.height
+            limitation_validator.weight = visitor.weight
+            return True
+        except (ValueError, TypeError):
+            return False
